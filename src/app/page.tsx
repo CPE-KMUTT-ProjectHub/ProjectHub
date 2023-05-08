a8c84f57d295d3752a12db40c266e8ff79323a70
Wittawat Kitipatthavorn <earthwittawat2005@gmail.com>
2023-05-08 09:09:58 +0700

    first commit🥹

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+    <div className='text-2xl font-bold'>
+      KMUTT Project HUB
+    </div>
+    <br/>
+    <div className='text-2xl font-bold'>
+      Coming Soon...
+    </div>
+    <div className='text-md pt-24'>
+    <a href='https://github.com/CPE-KMUTT-ProjectHub'>GitHub</a>
+    </div>
+    </main>
+  )
+}
