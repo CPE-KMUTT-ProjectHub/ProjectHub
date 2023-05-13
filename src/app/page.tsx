eea0094e6672a598d2ae12af8c145e54450ee1d0
Jedsadaporn pannok <tikpop555@gmail.com>
2023-05-13 21:44:30 +0700

    Github Button Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 import Link from '@/components/Link'
 import logo from '@/images/KMUTT_LOGO.png'
 import Image from 'next/image'
-import { GoMarkGithub } from "react-icons/go";
-
 
 const Home: React.FC = (): JSX.Element => {
   return (
@@ -10,16 +8,9 @@ const Home: React.FC = (): JSX.Element => {
       <Image width={100} src={logo} alt="kmutt-logo" priority />
       <div className="text-2xl">KMUTT Project HUB</div>
       <div className="text-xl">Coming Soon...</div>
-      <div className="github">
-      <a href="https://github.com/CPE-KMUTT-ProjectHub" target='_black'><GoMarkGithub/> Github </a>
-      </div>
-     
-
-    
-      
+      <Link to="https://github.com/CPE-KMUTT-ProjectHub">Github</Link>
     </main>
   )
-  
 }
 
 export default Home
