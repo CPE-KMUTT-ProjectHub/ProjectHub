2eb25dc2a9b51bcaf24bb8ff477f78f800cadd05
Jedsadaporn pannok <tikpop555@gmail.com>
2023-05-13 21:45:53 +0700

    update

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -3,7 +3,7 @@ import { GoMarkGithub } from "react-icons/go";
 
 const Link: React.FC<Props> = ({ to, children }: Props): JSX.Element => {
   return (
-    <a className="text-sm no-underline no-border mt-5" href={to} target='_black'>
+    <a className="text-sm no-underline no-border mt-5" href={to}>
       <button className="bg-white-500 hover:bg-white-700 transition ease-in-out rounded px-3 py-2"><GoMarkGithub/> {children}</button>
     </a>
   )
