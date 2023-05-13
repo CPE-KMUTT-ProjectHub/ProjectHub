b78496c7120a778252cbb6ef8ebf9634e9af5121
Jedsadaporn pannok <tikpop555@gmail.com>
2023-05-13 22:55:20 +0700

    Github Button
    Github Button

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -1,9 +1,10 @@
 import { ReactNode } from 'react'
+import { GoMarkGithub } from "react-icons/go";
 
 const Link: React.FC<Props> = ({ to, children }: Props): JSX.Element => {
   return (
-    <a className="text-sm no-underline no-border mt-10" href={to}>
-      <button className="bg-cyan-500 hover:bg-cyan-700 transition ease-in-out rounded px-3 py-2">{children}</button>
+    <a className="text-sm no-underline no-border mt-5" href={to}>
+      <button className="bg-white-500 hover:bg-white-700 transition ease-in-out rounded px-3 py-2"><GoMarkGithub /> {children}</button>
     </a>
   )
 }
