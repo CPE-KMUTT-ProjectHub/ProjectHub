4c727c013745d009b51bc9dbfee72740c0f3cbeb
Jedsadaporn pannok <tikpop555@gmail.com>
2023-05-14 07:01:33 +0700

    testtttttt
    testtttttt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ const Home: React.FC = (): JSX.Element => {
     <main className="flex min-h-screen flex-col items-center justify-center gap-2">
       <Image width={100} src={logo} alt="kmutt-logo" priority />
       <div className="text-2xl">KMUTT Project HUB</div>
-      <div className="text-xl">Coming Soon....</div>
+      <div className="text-xl">Coming Soon......</div>
       <Link to="https://github.com/CPE-KMUTT-ProjectHub">Github</Link>
     </main>
   )
