7dc5fcb00330b40b80e3a027f3cb2a03b0c60d59
Jedsadaporn pannok <tikpop555@gmail.com>
2023-05-14 22:47:10 +0700

    fix(repair): Github Button
    fix(repair): Github Button

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -1,9 +1,9 @@
 import { ReactNode } from 'react'
 
-const Link: React.FC<Props> = ({ to, children }: Props): JSX.Element => {
+const Link: React.FC<Props> = ({ to, children, target }: Props): JSX.Element => {
   return (
-    <a className="text-sm no-underline no-border mt-10" href={to}>
-      <button className="bg-cyan-500 hover:bg-cyan-700 transition ease-in-out rounded px-3 py-2">{children}</button>
+    <a className="text-sm no-underline no-border mt-5" href={to} target={target}>
+      <button className="bg-white-500 hover:bg-white-700ß transition ease-in-out rounded px-3 py-2">{children}</button>
     </a>
   )
 }
@@ -13,4 +13,5 @@ export default Link
 type Props = {
   to: string
   children: ReactNode
+  target: string
 }
