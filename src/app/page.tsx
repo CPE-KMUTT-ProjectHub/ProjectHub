d22691ebe1ae8db706119b5dc958d65e6946c45c
Wittawat Kitipatthavorn <earthwittawat2005@gmail.com>
2023-07-18 01:51:55 +0700

    landing page + navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,28 @@
 import Link from '@/components/Link'
-import logo from '@/images/KMUTT_LOGO.png'
 import Image from 'next/image'
-import { GoMarkGithub } from 'react-icons/go'
-
+import Navbar from '@/components/navbar/navbar'
+import { ChevronDownIcon, MagnifyingGlassIcon } from '@heroicons/react/20/solid'
 const Home: React.FC = (): JSX.Element => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-2">
-      <Image width={100} src={logo} alt="kmutt-logo" priority />
-      <div className="text-2xl">KMUTT Project HUB</div>
-      <div className="text-xl">Coming Soon....</div>
-      <Link to="https://github.com/CPE-KMUTT-ProjectHub" target="_black">
-        <div className="flex flex-row gap-1 items-center justify-center">
-          <GoMarkGithub />
-          Github
+    <>
+      <Navbar />
+      <main className="flex bg-white min-h-screen flex-col items-center gap-2">
+        <div className="text-2xl lg:text-4xl font-bold text-gray-800 mt-10">KMUTT Project HUB</div>
+        <img className="mt-4 w-[20rem] lg:w-[40rem]" src="/heropage/heroimg.png" alt="hero-img" />
+        <div className="mt-4 relative">
+          <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+            <MagnifyingGlassIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+          </div>
+          <input
+            id="search"
+            name="search"
+            className="block w-[20rem] lg:w-[40rem] rounded-full border-0 bg-white py-1.5 pl-10 pr-3 text-sm text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:leading-6"
+            placeholder="Search"
+            type="search"
+          />
         </div>
-      </Link>
-    </main>
+      </main>
+    </>
   )
 }
 
---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,82 @@
+'use client';
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import Link from 'next/link'
+const navigation = [
+  { name: 'Sign in', href: '/signin', current: true },
+]
+
+function classNames(...classNamees: string[]) {
+  return classNamees.filter(Boolean).join(' ')
+}
+
+export default function navbar() {
+  return (
+    <Disclosure as="nav" className="bg-white">
+      {({ open }) => (
+        <>
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex flex-shrink-0 items-center">
+                    <Link className='text-black text-2xl font-bold' href={'/'}>CPE 🐜</Link>
+                </div>
+              
+              </div>
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+              <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                      <Link
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          'text-white bg-[#4A60AC] hover:bg-gray-700 hover:text-white',
+                          'rounded-full px-3 py-2 text-sm font-medium'
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
+                      >
+                        {item.name}
+                      </Link>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="space-y-1 px-2 pb-3 pt-2">
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    'text-gray-900 hover:bg-gray-700 hover:text-white',
+                    'block rounded-md px-3 py-2 text-base font-medium'
+                  )}
+                  aria-current={item.current ? 'page' : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  )
+}
