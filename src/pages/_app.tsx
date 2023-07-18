6bbe15514a90f9df6af8fdea771b86b5eaed38bb
Mirailisc <mirailisclm@gmail.com>
2023-07-18 14:26:24 +0700

    sign in page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,11 +5,25 @@ import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
 import { Suspense } from 'react'
 import '../app/globals.css'
+import Navbar from '@/components/Navbar'
+import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const location = useRouter()
+
+  const renderNavbar = () => {
+    switch (location.pathname) {
+      case '/auth/sign-in':
+        return
+      default:
+        return <Navbar />
+    }
+  }
+
   return (
     <Suspense fallback={<Loading />}>
       <SessionProvider session={pageProps.session}>
+        {renderNavbar()}
         <Component {...pageProps} />
       </SessionProvider>
     </Suspense>
