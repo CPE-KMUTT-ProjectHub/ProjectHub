3c85ea2f8aaf7edc12a14f9540c881e965c0c2d7
Phubordin Poolnai <mirailisclm@gmail.com>
2023-07-21 19:14:06 +0700

    try fixing SessionProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,22 @@
-import { Montserrat, Mulish, Nunito } from "next/font/google";
-import "./globals.css";
+import { Montserrat, Mulish, Nunito } from 'next/font/google'
+import './globals.css'
 
 const montserrat = Montserrat({
-  variable: "--font-montserrat",
-  subsets: ["latin"],
-});
-const nunito = Nunito({ variable: "--font-nunito", subsets: ["latin"] });
-const mulish = Mulish({ variable: "--font-mulish", subsets: ["latin"] });
+  variable: '--font-montserrat',
+  subsets: ['latin'],
+})
+const nunito = Nunito({ variable: '--font-nunito', subsets: ['latin'] })
+const mulish = Mulish({ variable: '--font-mulish', subsets: ['latin'] })
 
 export const metadata = {
-  title: "Project Hub KMUTT",
-  description: "Explore new possibility",
-};
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body
-        className={`${montserrat.variable} ${nunito.variable} ${mulish.variable} overscroll-none`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+  title: 'Project Hub KMUTT',
+  description: 'Explore new possibility',
 }
 
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={`${montserrat.variable} ${nunito.variable} ${mulish.variable} overscroll-none`}>{children}</body>
+    </html>
+  )
+}
---
+++ src/components/AuthProvider/index.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { SessionProvider } from 'next-auth/react'
+import { ReactNode } from 'react'
+
+type Props = {
+  children?: ReactNode
+  session: any
+}
+
+const AuthProvider = ({ children, session }: Props) => {
+  return <SessionProvider session={session}>{children}</SessionProvider>
+}
+
+export default AuthProvider
